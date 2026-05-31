a1b232918fec5e643cce01329cdae3afec1b8b82
andrius <andrius.solopovas@gmail.com>
2026-05-31 20:23:03 +0100

    Update generated libwkhtmltox docs for header/footer margin

--- docs/libwkhtmltox/search/variables_7.js
+++ docs/libwkhtmltox/search/variables_7.js
@@ -1,4 +1,18 @@
-var searchData=
-[
-  ['margin_126',['margin',['../structwkhtmltopdf_1_1settings_1_1HeaderFooter.html#a7e1a03e3f09e91334e21c0a1c5efbe8e',1,'wkhtmltopdf::settings::HeaderFooter::margin()'],['../structwkhtmltopdf_1_1settings_1_1PdfGlobal.html#a5134abc2cf21870b455416d5efc466f6',1,'wkhtmltopdf::settings::PdfGlobal::margin()']]]
+var searchData = [
+	[
+		"margin_126",
+		[
+			"margin",
+			[
+				"../structwkhtmltopdf_1_1settings_1_1HeaderFooter.html#a7e1a03e3f09e91334e21c0a1c5efbe8e",
+				1,
+				"wkhtmltopdf::settings::HeaderFooter::margin()",
+			],
+			[
+				"../structwkhtmltopdf_1_1settings_1_1PdfGlobal.html#a5134abc2cf21870b455416d5efc466f6",
+				1,
+				"wkhtmltopdf::settings::PdfGlobal::margin()",
+			],
+		],
+	],
 ];
